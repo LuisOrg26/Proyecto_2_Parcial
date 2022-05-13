e174148bf75f21a3e3b5b36a16bfc3ea3630272d
LuisOrg26 <91204064+LuisOrg26@users.noreply.github.com>
2022-05-12 23:45:50 -0500

    Cesar con Ver Mas

--- cesar_mejorado.py
+++ cesar_mejorado.py
@@ -1,33 +1,56 @@
 from tkinter import*
 import numpy as np
+from PIL import Image, ImageTk
 
 def execute():
     print(__name__)
     window = Tk()
     window.title("Cesar Mejorado")
-    window.configure(bg="black")
+    window.configure(bg="#F1EEE9")
     window.geometry("300x300")
     my_menu = Menu(window)
 
+    def label(window,texto,estilo = ("Arial",12)):
+        return Label(window, text=texto,bg="#F1EEE9",font=estilo,fg="black")
+    def entry(window,var,status="normal"):
+        return Entry(window,bg="#73777B",textvariable=var,state=status)
+    def btn(window,texto,comando):
+        return Button(window,text=texto,command=comando,bg="#EC994B",fg="black")
+
     def ver_mas():
-        pass
-    option_menu = Menu(my_menu,background="black",foreground="white",tearoff=0)
+        ver = Toplevel()
+        ver.title("Ver Mas")
+        ver.geometry("450x300")
+        ver.configure(bg="#F1EEE9")
+        ver_titulo = label(ver,"¿Qué es el cifrado Cesar?",estilo=("Times",20))
+        ver_titulo.pack()
+        exp1 = label(ver,"El cifrado Cesar es uno de los más simples y conocidas\n"
+                         "se basa en el cifrado monoalfabético y se considera\n"
+                         " un método débil de criptografía, ya que es fácil\n"
+                         " decodificar el mensaje debido a sus técnicas de\n"
+                         " seguridad mínimas.")
+        exp1.pack()
+        image = Image.open("cesar.png")
+        resize_image = image.resize((200, 132))
+        img = ImageTk.PhotoImage(resize_image)
+        cesar_img = Label(ver,image=img)
+        cesar_img.image = img
+        cesar_img.pack()
+
+        ver.mainloop()
+
+    option_menu = Menu(my_menu,background="#F1EEE9",foreground="black",tearoff=0)
     my_menu.add_cascade( label="Opciones", menu= option_menu)
     option_menu.add_command(label="Ver Mas",command=ver_mas)
     option_menu.add_separator()
     option_menu.add_command(label="Salir",command=window.quit)
     window.config(menu=my_menu)
 
-    def label(window,texto,estilo = ("Arial",12)):
-        return Label(window, text=texto,bg="black",font=estilo,fg="white")
-    def entry(window,var,status="normal"):
-        return Entry(window,bg="green",textvariable=var,state=status)
-    def btn(window,texto,comando):
-        return Button(window,text=texto,command=comando,bg="green",fg="white")
+
 
     palabra = StringVar()
     code = StringVar()
-    lbltitulo = label(window,"Cesar",("Arial",30)).pack()
+    lbltitulo = label(window,"Cesar Mejorado",("Arial",30)).pack()
     lblpalabra = label(window,"Palabra").pack()
     txtpalabra = entry(window,palabra)
     txtpalabra.pack()
@@ -49,6 +72,7 @@ def execute():
                 new_word += chr((((ord(i) + salto) - 97) % 26) + 97)
         palabra.set(new_word)
     def cesar_normal():
+        txtcodigo.delete(0,END)
         txtcodigo.configure(state="disable")
     def cesar_mejorado():
         txtcodigo.configure(state="normal")
@@ -58,8 +82,6 @@ def execute():
     btncesar_normal = btn(window,"Cesar",cesar_normal).pack(anchor="s",side="left")
     btncesar_mejorado = btn(window,"Cesar Mejorado",cesar_mejorado).pack(anchor="s",side="right")
     window.mainloop()
-
-
 
 
 if __name__ == "__main__":
